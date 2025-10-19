77996d6a36716f7c5c734407449aad54f98de414
Vadim Surugiu <vadimsurugiu@Vadims-MacBook-Air.local>
2025-10-19 16:26:56 +0300

    finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import { useState, useEffect } from "react";
-import CoinCard from "./components/CoinCard";
-import LimitSelector from "./components/LimitSelector";
-import FilterInput from "./components/FilterInput";
-import SortSelector from "./components/SortSelector";
-
+import { Routes, Route } from "react-router";
+import HomePage from "./pages/Home";
+import AboutPage from "./pages/about";
+import Header from "./components/Header";
+import NotFoundPage from "./pages/not-found";
+import CoinDetailsPage from "./pages/coin-details";
 const API_URL = import.meta.env.VITE_API_URL;
 
 const App = () => {
@@ -32,55 +33,31 @@ const App = () => {
     fetchCoins();
   }, [limit]);
 
-  const filteredCoins = coins
-    .filter((coin) => {
-      return (
-        coin.name.toLowerCase().includes(filter.toLowerCase()) ||
-        coin.symbol.toLowerCase().includes(filter.toLowerCase())
-      );
-    })
-    .slice()
-    .sort((a, b) => {
-      switch (sortBy) {
-        case "market_cap_desc":
-          return b.market_cap - a.market_cap;
-        case "market_cap_asc":
-          return a.market_cap - b.market_cap;
-        case "price_desc":
-          return b.current_price - a.current_price;
-        case "price_asc":
-          return a.current_price - b.current_price;
-        case "change_desc":
-          return b.price_change_percentage_24h - a.price_change_percentage_24h;
-        case "change_asc":
-          return a.price_change_percentage_24h - b.price_change_percentage_24h;
-        default:
-          return 0;
-      }
-    });
-
   return (
-    <div>
-      <h1>🚀Crypto Dash</h1>
-      {loading && <p>Loading...</p>}
-      {error && <div className="error">{error}</div>}
-
-      <div className="top-controls">
-        <FilterInput filter={filter} onFilterChange={setFilter} />
-        <LimitSelector limit={limit} onLimitChange={setLimit} />
-        <SortSelector sortBy={sortBy} onSortChange={setSortBy} />
-      </div>
-
-      {!loading && !error && (
-        <main className="grid">
-          {filteredCoins.length > 0 ? (
-            filteredCoins.map((coin) => <CoinCard key={coin.id} coin={coin} />)
-          ) : (
-            <p>No coins match your search.</p>
-          )}
-        </main>
-      )}
-    </div>
+    <>
+      <Header />
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <HomePage
+              coins={coins}
+              filter={filter}
+              setFilter={setFilter}
+              limit={limit}
+              setLimit={setLimit}
+              sortBy={sortBy}
+              setSortBy={setSortBy}
+              loading={loading}
+              error={error}
+            />
+          }
+        />
+        <Route path="/about" element={<AboutPage />} />
+        <Route path="/coin/:id" element={<CoinDetailsPage />} />
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
+    </>
   );
 };
 
---
+++ src/pages/about.jsx
@@ -0,0 +1,10 @@
+const AboutPage = () => {
+  return (
+    <div className="about">
+      <h1>About Crypto Dash</h1>
+      <p>This is the about page</p>
+    </div>
+  );
+};
+
+export default AboutPage;
